27b43cb583675a785121d385913f8b0322289d19
RIGUANG JIN <bosco@RIGUANGs-iMac.local>
2020-08-21 08:25:32 -0400

    landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,127 @@
-import React from 'react'
-import { FaCode } from "react-icons/fa";
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios';
+import { Icon, Col, Card, Row } from 'antd';
+import ImageSlider from '../../utils/ImageSlider';
+// import CheckBox from './Sections/CheckBox';
+// import RadioBox from './Sections/RadioBox';
+// import { continents, price } from './Sections/Datas';
+// import SearchFeature from './Sections/SearchFeature';
+
+const { Meta } = Card;
 
 function LandingPage() {
+
+    const [Products, setProducts] = useState([])
+    const [Skip, setSkip] = useState(0)
+    const [Limit, setLimit] = useState(8)
+    const [PostSize, setPostSize] = useState()
+    const [SearchTerms, setSearchTerms] = useState("")
+
+    const [Filters, setFilters] = useState({
+        continents: [],
+        price: []
+    })
+
+    useEffect(() => {
+
+        const variables = {
+            skip: Skip,
+            limit: Limit,
+        }
+
+        getProducts(variables)
+
+    }, []);
+
+
+    const getProducts = (variables) => {
+        Axios.post('/api/product/getProducts', variables)
+            .then(response => {
+                if (response.data.success) {
+                    if (variables.loadMore) {
+                        setProducts([...Products, ...response.data.products])
+                    } else {
+                        setProducts(response.data.products)
+                    }
+                    setPostSize(response.data.postSize)
+                } else {
+                    alert('Failed to fectch product datas')
+                }
+            })
+    }
+
+    const renderCards = Products.map((product, index) => {
+
+        return <Col lg={6} md={8} xs={24}>
+            <Card
+                hoverable={true}
+                cover={<a href={`/product/${product._id}`} > <ImageSlider images={product.images} /></a>}
+            >
+                <Meta
+                    title={product.title}
+                    description={`$${product.price}`}
+                />
+            </Card>
+        </Col>
+    })
+
+
+
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
+        <div style={{ width: '75%', margin: '3rem auto' }}>
+            <div style={{ textAlign: 'center' }}>
+                <h2>  Let's Travel Anywhere  <Icon type="rocket" />  </h2>
             </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+
+
+            {/* Filter  */}
+
+            <Row gutter={[16, 16]}>
+                <Col lg={12} xs={24} >
+                    {/* <CheckBox
+                        list={continents}
+                        // handleFilters={filters => handleFilters(filters, "continents")}
+                    /> */}
+                </Col>
+                <Col lg={12} xs={24}>
+                    {/* <RadioBox
+                        list={price}
+                        // handleFilters={filters => handleFilters(filters, "price")}
+                    /> */}
+                </Col>
+            </Row>
+
+
+            {/* Search  */}
+            <div style={{ display: 'flex', justifyContent: 'flex-end', margin: '1rem auto' }}>
+
+                {/* <SearchFeature
+                    // refreshFunction={updateSearchTerms}
+                /> */}
+
+            </div>
+
+
+            {Products.length === 0 ?
+                <div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
+                    <h2>No post yet...</h2>
+                </div> :
+                <div>
+                    <Row gutter={[16, 16]}>
+                        {renderCards}
+                    </Row>
+                </div>
+            }
+            <br /><br />
+
+            {PostSize >= Limit &&
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    {/* <button onClick={onLoadMore}>Load More</button> */}
+                </div>
+            }
+
+
+        </div>
     )
 }
 

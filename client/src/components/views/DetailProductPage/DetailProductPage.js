287bd9561888b86bf14459edba853286088e1b3e
RIGUANG JIN <bosco@RIGUANGs-iMac.local>
2020-08-22 11:41:30 -0400

    add cart

--- client/src/components/views/DetailProductPage/DetailProductPage.js
+++ client/src/components/views/DetailProductPage/DetailProductPage.js
@@ -3,7 +3,7 @@ import Axios from 'axios'
 import { Row, Col } from 'antd';
 import ProductImage from './Sections/ProductImage';
 import ProductInfo from './Sections/ProductInfo';
-// import { addToCart } from '../../../_actions/user_actions';
+import { addToCart } from '../../../_actions/user_actions';
 import { useDispatch } from 'react-redux';
 
 
@@ -22,7 +22,7 @@ function DetailProductPage(props) {
     }, [])
 
     const addToCartHandler = (productId) => {
-        // dispatch(addToCart(productId))
+        dispatch(addToCart(productId))
     }
 
     return (
